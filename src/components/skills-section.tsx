d17c1c71e614d1abd9b650dd1306dfc1463a9db6
Naffy Dharni <naffydharni006@gmail.com>
2024-05-28 19:43:01 +0530

    add link to praise button

--- src/components/skills-section.tsx
+++ src/components/skills-section.tsx
@@ -9,10 +9,12 @@ export function SkillsSection() {
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           Skills & Tools
         </h1>
-        <Button className="gap-2" variant="ghost">
-          Praise Me
-          <Mail className="h-5 w-5" />
-        </Button>
+        <a href="mailto:naffydharni006@gmail.com?subject=Your skills are amazing!&body=I was really impressed by your skills and would love to continue to admire you.">
+          <Button className="gap-2" variant="ghost">
+            Praise Me
+            <Mail className="h-5 w-5" />
+          </Button>
+        </a>
       </div>
       <div className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-10 place-items-center gap-6 md:gap-8">
         <SkillCard
