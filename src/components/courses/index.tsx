61b229ff4a8f0ee74dfe7c181179095fb9147108
Barinderpreet Singh <naffydharni006@gmail.com>
2025-04-25 14:06:09 +0530

    Remove hire me dialog and add linkedIn link

--- src/components/courses/index.tsx
+++ src/components/courses/index.tsx
@@ -1,14 +1,7 @@
 import { Building2 } from 'lucide-react'
-import { Button } from '../ui/button'
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from '../ui/dialog'
+import { buttonVariants } from '../ui/button'
 import { CourseCard } from './course-card'
+import { cn } from '@/utils'
 
 export default function CoursesSection() {
 	return (
@@ -17,24 +10,13 @@ export default function CoursesSection() {
 				<h1 className='scroll-m-20 text-4xl font-extrabold lg:text-5xl'>
 					Courses
 				</h1>
-				<Dialog>
-					<DialogTrigger asChild>
-						<Button className='gap-2' variant='ghost'>
-							Hire Me
-							<Building2 className='h-5 w-5' />
-						</Button>
-					</DialogTrigger>
-					<DialogContent>
-						<DialogHeader>
-							<DialogTitle>Oops!</DialogTitle>
-							<DialogDescription>
-								I'm not yet accepting any work from outsiders. But I can surely
-								help you if you if we are close enough. For that, contact me on
-								my personal number. You must have it, right?
-							</DialogDescription>
-						</DialogHeader>
-					</DialogContent>
-				</Dialog>
+				<a
+					href='https://linkedin.com/in/knownasnaffy'
+					className={cn(buttonVariants({ variant: 'ghost' }), 'gap-2')}
+				>
+					Hire Me
+					<Building2 className='h-5 w-5' />
+				</a>
 			</div>
 			<div className='grid lg:grid-cols-3 place-items-stretch gap-8'>
 				<CourseCard
@@ -56,7 +38,7 @@ export default function CoursesSection() {
 					org='BBSBEC, Fatehgarh Sahib'
 					duration='2024 - 2028'
 					major='Computer Science and Engineering'
-					progress={0}
+					progress={25}
 				/>
 			</div>
 		</section>
