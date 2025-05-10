d7c80e7835d7ddccb9b9f7c2c333751091177461
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-05-10 14:28:20 +0530

    Prepare basic layout

---
+++ app/components/personal-info.tsx
@@ -0,0 +1,124 @@
+import {
+  DatabaseIcon,
+  FileUser,
+  MailIcon,
+  MapPin,
+  Palette,
+  Rss,
+} from "lucide-react";
+import { GithubIcon, LinkedInIcon } from "~/lib/social-icons";
+
+export default function PersonalInfo() {
+  return (
+    <aside
+      className="border-r px-8 lg:px-12 xl:px-8 py-8 md:sticky md:top-0 md:h-screen space-y-6"
+      aria-label="Personal Info"
+    >
+      <section aria-labelledby="profile-heading">
+        <h2 id="profile-heading" className="text-2xl font-bold">
+          Barinderpreet Singh
+        </h2>
+        <p className="text-sm text-[#D1D5DC]">Software Engineer</p>
+      </section>
+
+      <SidebarSection
+        title="Contact"
+        items={
+          <>
+            <SidebarLink
+              href="https://en.wikipedia.org/wiki/Patiala"
+              newTab={true}
+            >
+              <MapPin className="size-4" />
+              <span className="sr-only">Location: </span>
+              Patiala, Punjab, India
+            </SidebarLink>
+            <SidebarLink href="mailto:naffydharni006@gmail.com" newTab={true}>
+              <MailIcon className="size-4" />
+              <span className="sr-only">Email: </span>
+              naffydharni006@gmail.com
+            </SidebarLink>
+          </>
+        }
+      />
+
+      <SidebarSection
+        title="Social"
+        items={
+          <>
+            <SidebarLink
+              href="https://linkedin.com/in/knownasnaffy"
+              newTab={true}
+            >
+              {LinkedInIcon}
+              LinkedIn
+            </SidebarLink>
+            <SidebarLink href="https://github.com/knownasnaffy" newTab={true}>
+              {GithubIcon}
+              GitHub
+            </SidebarLink>
+          </>
+        }
+      />
+
+      <SidebarSection
+        title="Quick Links"
+        items={
+          <>
+            <SidebarLink href="/blog" newTab={false}>
+              <Rss className="size-4" />
+              Blog
+            </SidebarLink>
+            <SidebarLink href="/resume" newTab={true}>
+              <Palette className="size-4" />
+              Fancy Page
+            </SidebarLink>
+          </>
+        }
+      />
+    </aside>
+  );
+}
+
+const SidebarSection = ({
+  title,
+  items,
+}: {
+  title: string;
+  items: React.ReactNode;
+}) => {
+  return (
+    <section
+      aria-labelledby={`${title}-heading`}
+      className="space-y-2 prose-sm"
+    >
+      <h2 id={`${title}-heading`} className="font-bold ">
+        {title}
+      </h2>
+      <ul className="mt-1 space-y-0 list-none pl-0">{items}</ul>
+    </section>
+  );
+};
+
+const SidebarLink = ({
+  href,
+  newTab,
+  children,
+}: {
+  href: string;
+  newTab: boolean;
+  children: React.ReactNode;
+}) => {
+  return (
+    <li className="pl-0">
+      <a
+        href={href}
+        target={newTab ? "_blank" : "_self"}
+        rel={newTab ? "noopener" : undefined}
+        className="text-sm text-[#D1D5DC] hover:text-primary inline-flex items-center gap-2"
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import PersonalInfo from "~/components/personal-info";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -10,7 +11,95 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <>
-      <main className="">Hello World</main>
+      <main className="relative max-w-7xl mx-auto grid md:grid-cols-[296px_1fr] lg:grid-cols-[340px_1fr] xl:grid-cols-[296px_1fr_250px] min-h-screen">
+        <PersonalInfo />
+        <div className="px-8 py-8 prose prose-sm dark:prose-invert prose-h1:mb-0 prose-h1:border-b prose-h1:pb-4 max-w-2xl mx-auto">
+          <h1 className="">Resume</h1>
+          <h2 id="about-heading" className="mt-6">
+            Professional Summary
+          </h2>
+          <p className="">
+            Self-taught <b>web developer</b> and B.Tech CSE student with{" "}
+            <b>2+ years</b> of hands-on experience building{" "}
+            <b>full-stack applications</b> and
+            <b> desktop software</b>. Strong foundation in <b>React</b>,
+            Next.js, and
+            <b> Tailwind</b>, with a growing interest in clean design systems,
+            CI/CD workflows, and developer tooling. Currently maintaining
+            multiple personal and academic projects while working part-time as a
+            freelance developer.
+          </p>
+          <h2 className="">Skills & Technologies</h2>
+          <ul className="">
+            <li>
+              <b>Frontend:</b> React, Next.js, Tailwind CSS, ShadCN, daisyUI,
+              Vite, React Router
+            </li>
+            <li>
+              <b>Backend:</b> Node.js (basic), REST APIs, JWT, OAuth
+            </li>
+            <li>
+              <b>State Management:</b> Redux, Zustand
+            </li>
+            <li>
+              <b>Tooling:</b> Bun, pnpm, Prettier, ESLint, Git, GitHub Actions
+            </li>
+            <li>
+              <b>Deployment:</b> GitHub Pages, Vercel, Custom Domain Management
+            </li>
+            <li>
+              <b>Design:</b> Figma, Semantic HTML, Accessibility Best Practices
+            </li>
+          </ul>
+        </div>
+        <aside className="sticky top-0 h-screen hidden lg:block border-l px-8 py-8">
+          <h3 id="toc" className=" font-semibold">
+            Table of Contents
+          </h3>
+          <ul className=" mt-2 space-y-1 text-sm">
+            <li>
+              <a
+                href="#section1"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Professional Higlights
+              </a>
+            </li>
+            <li>
+              <a
+                href="#section2"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Core Skills
+              </a>
+            </li>
+            <li>
+              <a
+                href="#section3"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="#section4"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Section 4
+              </a>
+            </li>
+            <li>
+              <a
+                href="#section5"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Section 5
+              </a>
+            </li>
+          </ul>
+        </aside>
+      </main>
     </>
   );
 }
