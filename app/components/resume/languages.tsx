75149da34e66e126f802641a46ba8d6ff4a92fe0
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-05-11 23:30:02 +0530

    Better logic for languages section

--- app/components/resume/languages.tsx
+++ app/components/resume/languages.tsx
@@ -1,20 +1,26 @@
 import { cn } from "~/lib/utils";
 
+const languages = [
+  { language: "Punjabi", proficieny: 5, remark: "Native" },
+  { language: "English", proficieny: 4, remark: "Professional" },
+  { language: "Hindi", proficieny: 4, remark: "Professional" },
+];
+
 export default function Languages() {
   const getSteps = (filled: number, total = 5) =>
     Array(filled)
       .fill(true)
       .concat(Array(total - filled).fill(false));
   return (
-    <>
-      <span className="grid gap-4">
-        <span>
+    <span className="grid gap-4">
+      {languages.map((item) => (
+        <span key={item.language}>
           <span className="flex justify-between w-full">
-            <h4 className="mt-0">Punjabi</h4>
-            <span>Native</span>
+            <h4 className="mt-0">{item.language}</h4>
+            <span>{item.remark}</span>
           </span>
           <span className="grid grid-cols-5 h-3 gap-2">
-            {getSteps(5).map((item, index) => (
+            {getSteps(item.proficieny).map((item, index) => (
               <span
                 className={cn(
                   item ? "bg-[#D1D5DC]" : "bg-muted",
@@ -26,43 +32,7 @@ export default function Languages() {
             ))}
           </span>
         </span>
-        <span>
-          <span className="flex justify-between w-full">
-            <h4 className="mt-0">English</h4>
-            <span>Professional</span>
-          </span>
-          <span className="grid grid-cols-5 h-3 gap-2">
-            {getSteps(4).map((item, index) => (
-              <span
-                className={cn(
-                  item ? "bg-[#D1D5DC]" : "bg-muted",
-                  "rounded-xs",
-                  index === 0 && "rounded-l-sm",
-                  index === 4 && "rounded-r-sm"
-                )}
-              ></span>
-            ))}
-          </span>
-        </span>
-        <span>
-          <span className="flex justify-between w-full">
-            <h4 className="mt-0">Hindi</h4>
-            <span>Professional</span>
-          </span>
-          <span className="grid grid-cols-5 h-3 gap-2">
-            {getSteps(4).map((item, index) => (
-              <span
-                className={cn(
-                  item ? "bg-[#D1D5DC]" : "bg-muted",
-                  "rounded-xs",
-                  index === 0 && "rounded-l-sm",
-                  index === 4 && "rounded-r-sm"
-                )}
-              ></span>
-            ))}
-          </span>
-        </span>
-      </span>
-    </>
+      ))}
+    </span>
   );
 }
