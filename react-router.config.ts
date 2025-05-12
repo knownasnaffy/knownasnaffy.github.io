59a7c001cee51593838532df5b460e746dc39a48
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-05-12 21:47:42 +0530

    Statically render the home page

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  async prerender() {
+    return ["/"];
+  },
   ssr: false,
 } satisfies Config;
